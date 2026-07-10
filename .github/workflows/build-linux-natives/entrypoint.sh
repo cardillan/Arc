ea0cd9a709ea88082e16f465df1809aa0beafd12
Anuken <arnukren@gmail.com>
2026-07-10 19:01:04 -0400

    ARM tests (2)

--- .github/workflows/build-linux-natives/entrypoint.sh
+++ .github/workflows/build-linux-natives/entrypoint.sh
@@ -37,4 +37,4 @@ apt-get -yq --force-yes install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf 
 apt-get -yq --force-yes install gcc-riscv64-linux-gnu g++-riscv64-linux-gnu libc6-dev-riscv64-cross
 
 # Build Linux natives
-./gradlew arc-core:jnigenBuildAllLinux extensions:filedialogs:jnigenBuildAllLinux --no-daemon
+./gradlew arc-core:jnigenBuildAllLinux extensions:filedialogs:jnigenBuildAllLinux extensions:freetype:jnigenBuildAllLinux --no-daemon
